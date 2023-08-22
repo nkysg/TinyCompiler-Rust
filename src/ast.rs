f6917f15a4cdf861bca7e9b9cb32a4f6e5746245
nk_ysg <nk_ysg@163.com>
2023-08-22 11:18:05 +0800

    implement Attr and TreeNode Display

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use crate::token::Token;
+use std::fmt::{Display, Formatter};
 
 #[derive(PartialEq, Debug)]
 pub enum NodeKind {
@@ -42,6 +43,17 @@ pub enum Attr {
     Name(String),
 }
 
+impl Display for Attr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Attr::Op(token) => write!(f, "{}", *token)?,
+            Attr::Val(val) => write!(f, "{}", *val)?,
+            Attr::Name(name) => write!(f, "{}", name)?,
+        }
+        Ok(())
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct TreeNode {
     pub child: Vec<Option<Box<TreeNode>>>,
@@ -49,7 +61,6 @@ pub struct TreeNode {
 
     // XXX FIXME YSG
     pub line_number: i32,
-    node_kind: NodeKind,
     pub expression_type: ExpressionType,
     pub kind: Kind,
     pub attr: Attr,
@@ -62,7 +73,6 @@ impl TreeNode {
             child,
             sibling: None,
             line_number: 0,
-            node_kind: NodeKind::StatementK,
             expression_type: ExpressionType::Void,
             kind: Kind::Statement(kind),
             attr: Attr::Val(0),
@@ -75,10 +85,55 @@ impl TreeNode {
             child,
             sibling: None,
             line_number: 0,
-            node_kind: NodeKind::ExpressionK,
             expression_type: ExpressionType::Void,
             kind: Kind::Expression(kind),
             attr: Attr::Val(0),
         }
+    }
+
+    // XXX FIXME YSG implement with loop
+    fn print_tree(&self, f: &mut Formatter<'_>, indent_count: &mut usize) -> std::fmt::Result {
+        *indent_count += 2;
+        writeln!(f, "cnt = {} ", *indent_count)?;
+
+        {
+            let str = std::iter::repeat(" ").take(*indent_count).collect::<String>();
+
+            match &self.kind {
+                Kind::Statement(stmt) => match stmt {
+                    StatementKind::IfK => writeln!(f, "{} If", str)?,
+                    StatementKind::RepeatK => writeln!(f, "{} Repeat", str)?,
+                    StatementKind::AssignK => writeln!(f, "{} Assign to: {}", str, self.attr)?,
+                    StatementKind::ReadK => writeln!(f, "{} Read: {}", str, self.attr)?,
+                    StatementKind::WriteK => writeln!(f, "{} Write", str)?,
+                },
+                Kind::Expression(expr) => match expr {
+                    ExpressionKind::Opk => writeln!(f, "{} Op: {}", str, self.attr)?,
+                    ExpressionKind::ConstK => writeln!(f, "{} const: {}", str, self.attr)?,
+                    ExpressionKind::IdK => writeln!(f, "{} Id: {}", str, self.attr)?,
+                },
+            }
+
+
+            for elem in self.child.iter() {
+                if let Some(node) = elem {
+                    node.print_tree(f, indent_count)?;
+                }
+            }
+
+            if let Some(node) = &self.sibling {
+                node.print_tree(f, indent_count)?;
+            }
+        }
+        *indent_count -= 2;
+        Ok(())
+    }
+}
+
+impl Display for TreeNode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut i = 0;
+        self.print_tree(f, &mut i)?;
+        Ok(())
     }
 }
